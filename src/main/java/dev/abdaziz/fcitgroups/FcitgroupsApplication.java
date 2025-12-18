4b8edf8378d8aab7f82ec06802e2a4f25c8a89c7
Abdulaziz <me@abdaziz.dev>
2025-12-18 11:37:50 +0300

    Initial commit

---
+++ src/main/java/dev/abdaziz/fcitgroups/FcitgroupsApplication.java
@@ -0,0 +1,13 @@
+package dev.abdaziz.fcitgroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcitgroupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcitgroupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abdaziz/fcitgroups/FcitgroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abdaziz.fcitgroups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcitgroupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
